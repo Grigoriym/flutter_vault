d488cac3267bd559a4a6a60f9b37ea1f0a5bfb7a
Mikhalchuk Grigoriy <grigoriymikhalchuk@hotmail.com>
2019-11-25 20:39:51 +0600

    Add db and models for notesList segment.

---
+++ lib/src/ui/noteslist/notes_list_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class NotesList extends StatefulWidget {
+  _NotesListState createState() => _NotesListState();
+}
+
+class _NotesListState extends State<NotesList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
